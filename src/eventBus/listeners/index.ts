7d39ddcc139b63d53de29c0742d681dc98b05470
Michal <mwargan@gmail.com>
2022-11-18 11:49:33 +0100

    Added analytics as listener

--- src/eventBus/listeners/index.ts
+++ src/eventBus/listeners/index.ts
@@ -5,11 +5,13 @@ import console from "./console";
 import notifications from "./notifications";
 import broadcastChannel from "./broadcastChannel";
 import appAnalytics from "./appAnalytics";
+import analytics from "./analytics";
 // import postMessage from "./postMessage";
 
 export default new Listeners(
   notifications,
   broadcastChannel,
   console,
+  analytics,
   appAnalytics
 );
